161b2e957fe82b114607f139129a4b8cb58a937a
Thunderpoet <thunderpoet@gmail.com>
2026-06-16 13:55:33 +0200

    Fix: logo image width 100% to fill header space on mobile

--- app/helpers/branding_helper.rb
+++ app/helpers/branding_helper.rb
@@ -11,7 +11,7 @@ module BrandingHelper
   end
 
   def _logo_as_symbol_wordmark
-    image_tag('/miacivezza-sun-small.png', alt: 'Mia Civezza', class: 'logo logo--wordmark', style: 'height: 50px; width: auto;')
+    image_tag('/miacivezza-sun-small.png', alt: 'Mia Civezza', class: 'logo logo--wordmark', style: 'width: 100%; max-width: 320px; height: auto; display: block; margin: 0 auto;')
   end
 
   def _logo_as_symbol_icon
--- app/javascript/flavours/glitch/features/closed_registrations_modal/index.jsx
+++ app/javascript/flavours/glitch/features/closed_registrations_modal/index.jsx
@@ -9,7 +9,7 @@ class ClosedRegistrationsModal extends ImmutablePureComponent {
     return (
       <div className='modal-root__modal interaction-modal'>
         <div className='interaction-modal__lead'>
-          <img src='/miacivezza-sun-small.png' alt='Mia Civezza' style={{ height: '60px', width: 'auto', marginBottom: '12px' }} />
+          <img src='/miacivezza-sun-small.png' alt='Mia Civezza' style={{ width: '100%', height: 'auto', display: 'block', marginBottom: '16px' }} />
           <h3><FormattedMessage id='closed_registrations_modal.title' defaultMessage='Signing up on MiaCivezza.com' /></h3>
           <p>
             <FormattedMessage
